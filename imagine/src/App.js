c96df46a3574f495093d84cac370135d441b4e3f
atatum718 <54254894+atatum718@users.noreply.github.com>
2020-10-09 19:59:27 -0400

    added folders for each screen, added code for gallery screen, installed react, installed fontawesome, installed browserRouter, installed styled components, added a folder that has a styled component for a button that will be on two screens, imported the screens on app.js

---
+++ imagine/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import "./App.css";
+import Filter from "./filter";
+import Gallery from "./gallery/Gallery";
+import Preview from "./preview";
+
+function App() {
+  return (
+    <div className="App">
+      <Gallery />
+    </div>
+  );
+}
+
+export default App;
